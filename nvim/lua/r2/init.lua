95f9ee0688c38f2dab6cdc2c3edbba8ae06b32f1
Arthur Moreira de Deus <arthur.md@parade.ai>
2024-04-30 18:25:55 -0300

    feat: Add cmp

--- nvim/lua/r2/init.lua
+++ nvim/lua/r2/init.lua
@@ -3,4 +3,5 @@ require("r2.remap")
 require("r2.options")
 require("r2.config.treesitter")
 require("r2.config.colors")
+require("r2.config.cmp")
 
--- nvim/lua/r2/plugins.lua
+++ nvim/lua/r2/plugins.lua
@@ -24,5 +24,15 @@ require("lazy").setup({
       "nvim-treesitter/nvim-treesitter",
       build = ":TSUpdate",
   },
+  {
+      "hrsh7th/nvim-cmp",
+      dependencies = {
+          "neovim/nvim-lspconfig",
+          "hrsh7th/cmp-nvim-lsp",
+          "hrsh7th/cmp-buffer",
+          "hrsh7th/cmp-path",
+          "hrsh7th/cmp-cmdline",
+  	}
+  },
 })
 
