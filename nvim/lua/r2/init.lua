eecbce8af6fb894c7f2f0d7d0505f7609e2b86d0
Arthur Moreira de Deus <arthur.md@parade.ai>
2024-04-30 17:24:02 -0300

    feat: Install treesitter and add color theme

--- nvim/lua/r2/init.lua
+++ nvim/lua/r2/init.lua
@@ -1,4 +1,6 @@
 require("r2.plugins")
 require("r2.remap")
 require("r2.options")
+require("r2.config.treesitter")
+require("r2.config.colors")
 
--- nvim/lua/r2/plugins.lua
+++ nvim/lua/r2/plugins.lua
@@ -18,6 +18,11 @@ require("lazy").setup({
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
       dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { 
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+  },
 })
 
